06228a8b4a047468d4595d19a2885c8a577c3ff3
badhon424 <badhon424@gmail.com>
2024-01-15 18:53:16 +0600

    preparing folder structures for mvc

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => 'your_database_host',
+    'dbname' => 'your_database_name',
+    'username' => 'your_database_username',
+    'password' => 'your_database_password',
+];
